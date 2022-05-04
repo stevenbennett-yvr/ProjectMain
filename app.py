0599d606d1bb4d560282e61c3b82369bb6a40188
Uluc <ubulbirol@yahoo.com>
2022-05-04 01:51:09 -0700

    changed calculator app to return instead of use calculation.grade, fixed issues with literal string error in calculation. calculator works to average out a list of grades and return a GPA

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 # Author: Desig9 by Deadline
 # Date: April 27, 2022
 # Flask API for GPA Calculator
-
+from __future__ import print_function
 from flask import Flask, render_template, request, jsonify
 import json
 from calculator import calculation
+import sys
+
 
 app = Flask(__name__)
 
@@ -16,51 +18,51 @@ def homepage():
 def send():
     if request.method == 'POST':
         grades = list()
-        course1 = request.form['course1']
-        course2 = request.form['course2']
-        course3 = request.form['course3']
-        course4 = request.form['course4']
-        course5 = request.form['course5']
-        course6 = request.form['course6']
-        course7 = request.form['course7']
-        course8 = request.form['course8']
+        course1 = int(request.form['course1'])
+        course2 = int(request.form['course2'])
+        course3 = int(request.form['course3'])
+        course4 = int(request.form['course4'])
+        course5 = int(request.form['course5'])
+        course6 = int(request.form['course6'])
+        course7 = int(request.form['course7'])
+        course8 = int(request.form['course8'])
 
-        calculation(course1)
-        grade1 = calculation.grade
+        # print(f'{int(course1)}', file=sys.stderr)
+        # print(course2, file=sys.stderr)
+        grade1 = calculation(course1)
         grades.append(grade1)
-
-        calculation(course2)
-        grade2 = calculation.grade
+        
+        grade2 = calculation(course2)
         grades.append(grade2)
 
-        calculation(course3)
-        grade3 = calculation.grade
+       
+        grade3 = calculation(course3)
         grades.append(grade3)
 
-        calculation(course4)
-        grade4 = calculation.grade
+        
+        grade4 = calculation(course4)
         grades.append(grade4)
 
-        calculation(course5)
-        grade5 = calculation.grade
+        
+        grade5 = calculation(course5)
         grades.append(grade5)
 
-        calculation(course6)
-        grade6 = calculation.grade
+        
+        grade6 = calculation(course6)
         grades.append(grade6)
 
-        calculation(course7)
-        grade7 = calculation.grade
+        
+        grade7 = calculation(course7)
         grades.append(grade7)
 
-        calculation(course8)
-        grade8 = calculation.grade
+        
+        grade8 = calculation(course8)
         grades.append(grade8)
 
         final_grade = sum(grades)/len(grades)
         final_gpa = round(final_grade, 2)
 
-        return render_template('demo.html', title='Demo', final_gpa = final_gpa)
+        return render_template('demo.html', title='Demo', final_gpa = final_gpa, course1=grade1,course=grade2)
     else:
         return render_template('demo.html', title='Demo')
 
