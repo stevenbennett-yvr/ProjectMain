8371319eeb460df4c21ba908e4abde08288c3bb1
Steven <stevenbennett90@gmail.com>
2022-05-10 15:04:07 -0700

    got mongodb to function

--- webapp.py
+++ webapp.py
@@ -1,22 +1,43 @@
-import sqlite3
-from flask import Flask, render_template
-# from flask_wtf import FlaskForm
-# from wtforms import StringField, FieldList, IntegerField, SubmitField
-# from wtforms.validators import DataRequired
+from flask import (
+    Flask,
+    render_template, 
+    redirect, 
+    flash, 
+    request
+)
+from flask_pymongo import PyMongo
 from calculator import calculation
 from forms import GradesForm, UserForm
-from databaseclasses import Users
+from datetime import datetime
 
 app = Flask(__name__, template_folder='./templates')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+
 app.config['SECRET_KEY'] = "fuckit"
+app.config['MONGO_URI'] = 'mongodb+srv://acit2911:acit2911@cluster0.nrjoq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+mongo = PyMongo(app)
+db = mongo.db
 
+@app.route('/')
+def homepage():
+    return render_template("homepage.html", title = "Layout Page")
 
-@app.route('/user/add', methods=['GET', 'POST'])
+@app.route('/user/add', methods=['POST', 'GET'])
 def add_user():
-    form = UserForm()
-    return render_template('add_user.html', form=form)
+    if request.method == 'POST':
+        form = UserForm(request.form)
+        user_name = form.name.data
+        user_email = form.email.data
 
+        db.agile.users.insert_one({
+            "name": user_name,
+            "email": user_email,
+            "date_added": datetime.utcnow()
+        })
+        flash("User successfully added", "success")
+        return redirect('/')
+    else:
+        form = UserForm()
+    return render_template('add_user.html', form=form)
 
 # How does this use POST?
 @app.route('/', methods=['GET', 'POST'])
@@ -31,8 +52,8 @@ def index():
             total.append(gpa)
         final_grade = sum(total)/len(total)
         final_gpa = round(final_grade, 2)
-        return render_template('test.html', username=form.username.data, courses=courses, gpa=final_gpa, grades=total, form=form)
-    return render_template('test.html', form=form)
+        return render_template('gpa_calc.html', username=form.username.data, courses=courses, gpa=final_gpa, grades=total, form=form)
+    return render_template('gpa_calc.html', form=form)
 
 
 if __name__ == "__main__":
